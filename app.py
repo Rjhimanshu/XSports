2b8d5283c2e05a757f656c3dda728ef76c238343
Rjhimanshu <rjhimanshu198@gmail.com>
2023-07-25 07:43:48 +0000

    Second Commit

--- app.py
+++ app.py
@@ -29,11 +29,12 @@ def home():
   cart = session.get('cart', [])
   # Check if user is logged in
   if 'email' in session:
+    session['cart'] = cart
     firstname = session['firstname']
-    return render_template('index.html', firstname=firstname)
+    return render_template('index.html', firstname=firstname, cart=cart)
   
   
-  return render_template('index.html', cart=cart)
+  return render_template('index.html')
 
 # Signup page
 @app.route('/signup', methods=['GET', 'POST'])
@@ -144,7 +145,48 @@ def products():
 
     return render_template('products.html', products=products, cart=cart)
 
+# Add to Cart
+@app.route('/add_to_cart/<int:product_id>/<int:quantity>', methods=['POST'])
+def add_to_cart(product_id, quantity):
+    # Convert product_id to string before storing in session
+    product_id_str = str(product_id)
+    
+    # Connect to Azure MySQL Database
+    conn = mysql.connector.connect(**product_management_config)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM products WHERE id = %s', (product_id,))
+    product = cursor.fetchone()
+    conn.close()
+    
+    cart = session.get('cart', [])
 
+    if product:
+        for item in cart:
+            if item['id'] == product_id_str:
+                # Product is already in the cart, update the quantity
+                item['quantity'] += quantity
+                break
+        else:
+            # Product is not in the cart, add it as a new item
+            item = {
+                'id': product_id_str,
+                'name': product['name'],
+                'price': product['price'],
+                'quantity': quantity
+            }
+            cart.append(item)
+
+        # Update the cart in the session
+        session['cart'] = cart
+
+        flash('Product added to cart successfully!', 'success')
+        return {'success': True}
+    else:
+        flash('Product not found!', 'error')
+        return {'success': False}
+
+
+'''
 # Add to Cart
 @app.route('/add_to_cart/<int:product_id>/<int:quantity>', methods=['POST'])
 def add_to_cart(product_id, quantity):
@@ -167,13 +209,7 @@ def add_to_cart(product_id, quantity):
             'price': product['price'],
             'quantity': quantity
         }
-        '''
-         # Check if the product is already in the cart
-        for item in cart:
-          if item['id'] == product[0]:
-              flash('Product is already added to the cart!', 'warning')
-              return redirect(url_for('products', product_id=product[0]))
-              '''
+      
 
         # Retrieve the cart from the session or initialize it if empty
         cart = session.get('cart', [])
@@ -188,14 +224,15 @@ def add_to_cart(product_id, quantity):
     else:
         flash('Product not found!', 'error')
         return {'sucess': False}
-'''
+ '''
+
 # Update Quantity
 @app.route('/update_quantity/<int:product_id>/<action>', methods=['POST'])
 def update_quantity(product_id, action):
     cart = session.get('cart', [])
 
     for item in cart:
-        if item['id'] == product_id:
+        if item['id'] == str(product_id):
             if action == 'increase':
                 item['quantity'] += 1
             elif action == 'decrease':
@@ -212,120 +249,14 @@ def remove_from_cart(product_id):
     cart = session.get('cart', [])
 
     for item in cart:
-        if item['id'] == product_id:
+        if item['id'] == str(product_id):
             cart.remove(item)
             break
 
     # Update the cart in the session
     session['cart'] = cart
     return {'success': True}
-'''
 
-# Update Cart Quantity
-@app.route('/update_quantity/<int:product_id>/<action>', methods=['POST'])
-def update_quantity(product_id, action):
-    print('Received request to update quantity for product:', product_id, 'Action:', action)
-    if 'user_id' not in session:
-        return jsonify({'success': False, 'message': 'User not logged in.'}), 403
-
-    user_id = session['user_id']
-    product_id = request.json.get('product_id')
-    quantity = request.json.get('quantity')
-
-    try:
-        # Connect to the 'product_management' database to update the cart quantity
-        conn = mysql.connector.connect(**product_management_config)
-        cursor = conn.cursor()
-
-        # Update the quantity in the 'order_details' table for the specific user and product
-        update_query = "UPDATE order_details SET quantity = %s WHERE user_id = %s AND id = %s"
-        cursor.execute(update_query, (quantity, user_id, product_id))
-        conn.commit()
-
-        # Close the database connection
-        cursor.close()
-        conn.close()
-
-        return jsonify({'success': True})
-
-    except Exception as e:
-        # Handle any errors that occur during database operations
-        return jsonify({'success': False, 'message': 'An error occurred while updating cart quantity.'}), 500
-
-
-# Remove Item from Cart
-@app.route('/remove_from_cart', methods=['POST'])
-def remove_from_cart():
-    if 'user_id' not in session:
-        return jsonify({'success': False, 'message': 'User not logged in.'}), 403
-
-    user_id = session['user_id']
-    product_id = request.json.get('product_id')
-
-    try:
-        # Connect to the 'product_management' database to remove the item from the cart
-        conn = mysql.connector.connect(**product_management_config)
-        cursor = conn.cursor()
-
-        # Delete the item from the 'order_details' table for the specific user and product
-        delete_query = "DELETE FROM order_details WHERE user_id = %s AND id = %s"
-        cursor.execute(delete_query, (user_id, product_id))
-        conn.commit()
-
-        # Close the database connection
-        cursor.close()
-        conn.close()
-
-        return jsonify({'success': True})
-
-    except Exception as e:
-        # Handle any errors that occur during database operations
-        return jsonify({'success': False, 'message': 'An error occurred while removing item from cart.'}), 500
-
-
-# Cart Page
-@app.route('/cart')
-def cart_page():
-    if 'email' in session:
-        # Get the user_id from the session
-        user_id = session.get('user_id')
-
-        # Check if the user is logged in and has a valid user_id
-        if not user_id:
-            flash('Please sign in to view your cart.', 'warning')
-            return redirect('/login')
-
-        try:
-            # Connect to the 'product_management' database to fetch the cart data
-            conn = mysql.connector.connect(**product_management_config)
-            cursor = conn.cursor(dictionary=True)
-
-            # Fetch the cart data from the database for the current user
-            query = "SELECT * FROM order_details WHERE user_id = %s"
-            cursor.execute(query, (user_id,))
-            cart = cursor.fetchall()
-
-            # Calculate the cart total
-            cart_total = sum(item['total_price'] for item in cart)
-
-            # Close the database connection
-            cursor.close()
-            conn.close()
-
-            return render_template('cart.html', cart=cart, cart_total=cart_total)
-
-        except Exception as e:
-            # Handle any errors that occur during database operations
-            flash('An error occurred while fetching your cart details.', 'danger')
-            print(f"Error: {e}")
-            return redirect(url_for('home'))
-
-    else:
-        # User is not logged in, redirect to the login page with a flash message
-        flash("Please sign in to view your cart.")
-        return redirect(url_for('login'))
-
-'''
 # Cart Page
 @app.route('/cart')
 def cart_page():
@@ -343,13 +274,12 @@ def cart_page():
 
     cart_total = cart_total.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
     
-    return render_template('cart.html', cart=cart, cart_total=cart_total, price=price, quantity=quantity)
+    return render_template('cart.html', cart=cart, cart_total=cart_total)
   
   else:
     # User is not logged in, redirect to the login page with a flash message
     flash("Please sign in to add products to the cart or view the cart.")
     return redirect(url_for('login'))
-  '''
 
 # Product description page
 @app.route('/products/<int:product_id>')
@@ -412,7 +342,8 @@ def address():
             
             cursor_product.close()
             conn_product.close()
-
+            
+            
             # Redirect to the payment page after successful data storage
             return redirect('/payment')
 
@@ -424,61 +355,49 @@ def address():
     return render_template('address.html')
 
 
-
 @app.route('/payment', methods=['GET', 'POST'])
 def payment():
-
-    # Retrieve the cart data from the session
     cart = session.get('cart', [])
     cart_total = Decimal(0)
 
-    # Calculate the total price of all items in the cart
     for item in cart:
         price = Decimal(item['price'])
         quantity = int(item['quantity'])
         item_total = price * quantity
         cart_total += item_total
 
-        cart_total = cart_total.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
-        
-        user_id = session.get('user_id')
+    cart_total = cart_total.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+    user_id = session.get('user_id')
 
-        # If payment is successful, store the order details in the database
+    if request.method == 'POST':
+        # If the form has been submitted, process the payment and store order details in the database
         try:
-            # Get the user_id from the session (assuming you have stored it during login)
-            
+            address_id = session.get('address_id')
+
             # Connect to the 'product_management' database to store the order details
             conn = mysql.connector.connect(**product_management_config)
             cursor = conn.cursor()
-            
-            print("Inserting order details into the database...")
-            
-            # Get the address_id from the session (assuming you have stored it during address submission)
-            address_id = session.get('address_id')
 
             # Insert the order details into the order_details table
             for item in cart:
-            
                 product_name = item['name']
                 price = Decimal(item['price'])
                 quantity = int(item['quantity'])
                 total_price = price * quantity
-               
+
                 # Prepare the SQL query for inserting data into the order_details table
-                
                 insert_query = (
-                "INSERT INTO order_details (user_id, address_id, product_name, quantity, price, total_price) VALUES (%s, %s, %s, %s, %s, %s)"
+                    "INSERT INTO order_details (user_id, address_id, product_name, quantity, price, total_price) VALUES (%s, %s, %s, %s, %s, %s)"
                 )
-                value = (user_id, address_id, product_name, quantity, price, total_price)
-                cursor.execute(insert_query, value)
-               
+                values = (user_id, address_id, product_name, quantity, price, total_price)
+                cursor.execute(insert_query, values)
+
             # Commit the changes to the database
-                conn.commit()
+            conn.commit()
 
             # Close the database connection
-                cursor.close()
-                conn.close()
-                
+            cursor.close()
+            conn.close()
 
             # Clear the cart from the session after successful order placement
             session['cart'] = []
@@ -493,7 +412,6 @@ def payment():
             print(f"Error: {e}")
 
     return render_template('payment.html', cart=cart, cart_total=cart_total)
-
 
 # Route for the Payment Success Page
 @app.route('/payment_success', methods=['GET'])
